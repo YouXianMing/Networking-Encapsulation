31f7c3710dbabfed895a3735192569573164e2e9
YouXianMing <705786299@qq.com>
2015-11-06 21:16:30 +0800

    2.7

---
+++ AFNetworking-3.x/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  AFNetworking-3.x
+//
+//  Created by YouXianMing on 15/11/6.
+//  Copyright © 2015年 YouXianMing. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    
+    [super viewDidLoad];
+}
+
+@end
---
+++ AFNetworking-Deprecated/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AFNetworking-Deprecated
+//
+//  Created by YouXianMing on 15/11/6.
+//  Copyright © 2015年 YouXianMing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
